5f3349e2c2c10cedd691dbafd344902121f5ca60
qpongratz <lagarathan@gmail.com>
2021-01-10 14:35:56 -0600

    Refactor dictionary class

--- lib/word_picker.rb
+++ lib/word_picker.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 # Parses the dictionary and prunes it for words of appropriate length
-class WordPicker
+class Dictionary
   attr_reader :dictionary
 
   def initialize
-    @dictionary = File.open('5desk.txt', 'r').read.split
-    @dictionary.select! { |word| word.length.between?(5, 12) }
+    @dictionary = File.open('5desk.txt', 'r')
+                      .read
+                      .split
+                      .select! { |word| word.length.between?(5, 12) }
   end
 end
