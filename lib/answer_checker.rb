356579249b37c0985bd573d12846f8269bd167fe
qpongratz <lagarathan@gmail.com>
2021-01-10 17:25:39 -0600

    Setup initial game class

--- lib/answer_checker.rb
+++ lib/answer_checker.rb
@@ -4,11 +4,8 @@ require_relative 'dictionary'
 
 # Keeps track of secret word and checks guessed letters
 class AnswerChecker
-  attr_reader :secret_word
-
   def initialize
     @word_list = Dictionary.new.dictionary
-    new_word
   end
 
   def new_word
@@ -16,40 +13,18 @@ class AnswerChecker
     @answer_state = Array.new(@secret_word.length, '_')
   end
 
-  def check_letter(guessed_letter)
+  def update_answer_state(guessed_letter)
     @secret_word.each_with_index do |letter, index|
       @answer_state[index] = letter if guessed_letter == letter.downcase
     end
     @answer_state
   end
 
+  def correct_guess?(guess)
+    @secret_word.include?(guess)
+  end
+
   def solved?
-    @answer_state.include?('_') ? false : true
+    @answer_state.none?('_')
   end
 end
-
-answer = AnswerChecker.new
-p answer.secret_word
-p answer.check_letter('a')
-p answer.check_letter('b')
-p answer.check_letter('c')
-p answer.check_letter('d')
-p answer.check_letter('e')
-p answer.check_letter('f')
-p answer.check_letter('g')
-p answer.check_letter('h')
-p answer.check_letter('i')
-p answer.check_letter('j')
-p answer.check_letter('k')
-p answer.check_letter('l')
-p answer.check_letter('m')
-p answer.check_letter('n')
-p answer.check_letter('o')
-p answer.check_letter('p')
-p answer.check_letter('t')
-p answer.check_letter('s')
-p answer.check_letter('r').join
-p answer.solved?
-answer.new_word
-p answer.solved?
-
--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,44 @@
 # frozen_string_literal: true
+
+require_relative 'answer_checker'
+require_relative 'player_input'
+require_relative 'display'
+
+# Handles general game flow.
+class Game
+  def initialize
+    @checker = AnswerChecker.new
+    new_game
+  end
+
+  def new_game
+    @incorrect_guesses = 0
+    @checker.new_word
+    @player = PlayerInput.new
+    @game_over = false
+    # display initial state
+  end
+
+  def play_turn
+    guess = @player.guess
+    @checker.correct_guess?(guess) ? correct_guess(guess) : incorrect_guess
+    unless @game_over == true
+      @checker.display_new_state
+      play_turn
+    end
+  end
+
+  def incorrect_guess
+    @incorrect_guesses += 1
+    end_game('lose') if @incorrect_guesses > 5
+  end
+
+  def correct_guess
+    answer_state = @checker.update_answer_state(guess)
+    end_game('win') if @checker.solved?
+  end
+
+  def end_game(state)
+    @game_over = true
+
+end
