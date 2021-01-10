aaa7c0d89c657ea3c6573df2463cd375d5a8dd65
qpongratz <lagarathan@gmail.com>
2021-01-10 16:28:29 -0600

    Create methods for player inputs class

--- lib/answer_checker.rb
+++ lib/answer_checker.rb
@@ -1,1 +1,55 @@
 # frozen_string_literal: true
+
+require_relative 'dictionary'
+
+# Keeps track of secret word and checks guessed letters
+class AnswerChecker
+  attr_reader :secret_word
+
+  def initialize
+    @word_list = Dictionary.new.dictionary
+    new_word
+  end
+
+  def new_word
+    @secret_word = @word_list.sample.split('')
+    @answer_state = Array.new(@secret_word.length, '_')
+  end
+
+  def check_letter(guessed_letter)
+    @secret_word.each_with_index do |letter, index|
+      @answer_state[index] = letter if guessed_letter == letter.downcase
+    end
+    @answer_state
+  end
+
+  def solved?
+    @answer_state.include?('_') ? false : true
+  end
+end
+
+answer = AnswerChecker.new
+p answer.secret_word
+p answer.check_letter('a')
+p answer.check_letter('b')
+p answer.check_letter('c')
+p answer.check_letter('d')
+p answer.check_letter('e')
+p answer.check_letter('f')
+p answer.check_letter('g')
+p answer.check_letter('h')
+p answer.check_letter('i')
+p answer.check_letter('j')
+p answer.check_letter('k')
+p answer.check_letter('l')
+p answer.check_letter('m')
+p answer.check_letter('n')
+p answer.check_letter('o')
+p answer.check_letter('p')
+p answer.check_letter('t')
+p answer.check_letter('s')
+p answer.check_letter('r').join
+p answer.solved?
+answer.new_word
+p answer.solved?
+
--- lib/player_input.rb
+++ lib/player_input.rb
@@ -1,1 +1,30 @@
 # frozen_string_literal: true
+
+# Gets player input and checks if valid.
+class PlayerInput
+  attr_reader :guesses
+
+  def initialize
+    @guesses = []
+  end
+
+  def guess
+    puts 'What letter would you like to guess?'
+    letter = gets.chomp.downcase
+    if valid?(letter)
+      @guesses.push(letter)
+      @letter = letter
+    else
+      puts 'Invalid guess.'
+      guess
+    end
+  end
+
+  private
+
+  def valid?(letter)
+    letter.between?('a', 'z') &&
+      letter.length == 1 &&
+      !@guesses.include?(letter)
+  end
+end
