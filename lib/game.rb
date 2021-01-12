95df18a072b26d8db528841aae1ac1af759335eb
qpongratz <lagarathan@gmail.com>
2021-01-12 13:53:33 -0600

    Clean comments

--- lib/game.rb
+++ lib/game.rb
@@ -55,22 +55,4 @@ class Game
     @game_over = true
     status == 'win' ? Display.win : Display.lose(@checker.secret_word)
   end
-
-  # def save_game
-  #   @file_name = "#{@player.player_name}-#{DateTime.now}.yml"
-  #   save_variables = {}
-  #   instance_variables.map do |var|
-  #     save_variables[var] = instance_variable_get(var)
-  #   end
-  #   Dir.mkdir('saves') unless File.exist? 'saves'
-  #   File.open("saves/#{file_name}", 'w') { |save| save.print(YAML.dump(save_variables)) }
-  # end
-
-  # def load_game(file_name)
-  #   loaded_variables = YAML.load(File.open("saves/#{file_name}", 'r').read)
-  #   loaded_variables.each do |key, value|
-  #     instance_variable_set(key, value)
-  #   end
-  #   resume_game
-  # end
 end
